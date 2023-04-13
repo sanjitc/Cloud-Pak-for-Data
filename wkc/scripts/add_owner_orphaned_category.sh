43cf6643a5b3022e23f8c184b5d925190430c405
Sanjit Chakraborty <17136230+sanjitc@users.noreply.github.com>
2023-04-13 15:56:30 -0500

    Update add_owner_orphaned_category.sh

--- wkc/scripts/add_owner_orphaned_category.sh
+++ wkc/scripts/add_owner_orphaned_category.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
 ################################
-# Recover orphaed category
+# Recover orphaned category
 #
 # POST /v3/categories/{CATEGORYID}/collaborators
 #
@@ -13,42 +13,4 @@ admin_uid=
 service_id_token=
 category_id=
 
-
-if [ "x${addr}" = "x" -o "x${admin_uid}" = "x" -o "x${service_id_token}" = "x" -o "x${category_id}" = "x" ]
-then
-  echo
-  echo "Need to fill-in addr, admin_uid, service_id_token, and category_id values in the script."
-  echo
-  exit 1
-fi
-
-addr=`echo ${addr} | sed -e 's/https\:\/\///'`
-addr=`echo ${addr} | sed -e 's/http\:\/\///'`
-
-
-token=${service_id_token}
-token_type=Basic
-
-curl -k -w "\n%{http_code}" -S -s -X POST \
-            -H "Accept: application/json" \
-            -H "Content-Type: application/json" \
-            -H "Authorization: ${token_type} ${token}" \
--d "{
-      \"members\": [
-        {
-          \"user_iam_id\": \"${admin_uid}\",
-          \"role\": \"admin\"
-        }
-      ]
-}" \
-"https://${addr}/v3/categories/${category_id}/collaborators" | {
-    read body
-    read code
-
-    if [ "x$code" = "x201" ]
-    then
-      echo "$body"
-    else
-      echo "ERROR:$code:$body"
-    fi
-}
+curl -X POST "https://${addr}/v3/categories/${category_id}/collaborators" -H  "accept: */*" -H  "Content-Type: */*" -H "Authorization: Basic ${service_id_token}" -d "{\"principal_id\":\"${admin_uid}\",\"role\":\"owner\",\"user_type\":\"USER\"}" -k
