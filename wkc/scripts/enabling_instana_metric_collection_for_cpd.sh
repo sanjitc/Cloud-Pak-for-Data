06e97f59b178d5faf9c8ed151ae59ed91dbb11bf
Sanjit Chakraborty <17136230+sanjitc@users.noreply.github.com>
2025-06-27 04:18:02 -0500

    Update enabling_instana_metric_collection_for_cpd.sh

--- wkc/scripts/enabling_instana_metric_collection_for_cpd.sh
+++ wkc/scripts/enabling_instana_metric_collection_for_cpd.sh
@@ -11,6 +11,12 @@ oc patch WS ws-cr -n ${NS}  --type merge --patch '{"spec": {"ignoreForMaintenanc
 # make the list of Node.js and GoLang microservices to enable for Instana
 export deployment_list="asset-files-api dap-dashboards-api datastage-ibm-datastage-assets datastage-ibm-datastage-canvas datastage-ibm-datastage-caslite datastage-ibm-datastage-codegen datastage-ibm-datastage-flows datastage-ibm-datastage-migration datastage-ibm-datastage-ruleset datastage-ibm-datastage-runtime dc-main event-logger-api jobs-api jobs-ui ngp-projects-api portal-catalog portal-common-api portal-dashboards portal-job-manager portal-main portal-ml-dl portal-notifications portal-projects usermgmt wdp-dataprep wml-main zen-core runtime-assemblies-operator runtime-manager-api"
 
+# save a copy of the deployments befome making changes
+for d in $deployment_list
+do
+oc get deployment/$d -n ${NS} -oyaml > $d.yaml
+done
+
 echo next, remove Instana env vars if any from Node.js microservices
 for d in $deployment_list
 do
