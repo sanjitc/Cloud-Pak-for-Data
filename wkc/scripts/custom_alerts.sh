390aff37516c1976a01541d91441814f0209758d
Sanjit Chakraborty <17136230+sanjitc@users.noreply.github.com>
2025-10-16 10:31:58 -0500

    Update custom_alerts.sh

--- wkc/scripts/custom_alerts.sh
+++ wkc/scripts/custom_alerts.sh
@@ -6,7 +6,7 @@
 couchdb_designdoc_reindexing() {
   oc exec wdp-couchdb-0 -c couchdb -- bash -c 'curl -ks -u "admin:`cat /etc/.secrets/COUCHDB_PASSWORD`" https://localhost:6984/_active_tasks' |jq '.[] | select((.type == "search_indexer") and (.design_document|endswith("_temporary")))' > /tmp/couchdb_designdoc_reindexing.out
 
-  if [ `stat -c %s /tmp/couchdb_designdoc_reindexing.out` -eq 0 ]; then
+  if [ `stat -c %s /tmp/couchdb_designdoc_reindexing.out` -ne 0 ]; then
 	  echo "file empty"
 	  mail -s "PRODUCTION: Warning! Reindexing in Progress for CouchDB Design Document" <email address> < /tmp/couchdb_designdoc_reindexing.out
   fi
