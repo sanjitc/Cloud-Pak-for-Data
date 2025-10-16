44eece96d0de05f4d0e860790798f37e2a5c50c3
Sanjit Chakraborty <17136230+sanjitc@users.noreply.github.com>
2025-10-16 10:35:16 -0500

    Update custom_alerts.sh

--- wkc/scripts/custom_alerts.sh
+++ wkc/scripts/custom_alerts.sh
@@ -4,7 +4,7 @@
 # More checks can be add in future.
 
 couchdb_designdoc_reindexing() {
-  oc exec wdp-couchdb-0 -c couchdb -- bash -c 'curl -ks -u "admin:`cat /etc/.secrets/COUCHDB_PASSWORD`" https://localhost:6984/_active_tasks' |jq '.[] | select((.type == "search_indexer") and (.design_document|endswith("_temporary")))' > /tmp/couchdb_designdoc_reindexing.out
+  oc exec -n <wkc/cpd project> wdp-couchdb-0 -c couchdb -- bash -c 'curl -ks -u "admin:`cat /etc/.secrets/COUCHDB_PASSWORD`" https://localhost:6984/_active_tasks' |jq '.[] | select((.type == "search_indexer") and (.design_document|endswith("_temporary")))' > /tmp/couchdb_designdoc_reindexing.out
 
   if [ `stat -c %s /tmp/couchdb_designdoc_reindexing.out` -ne 0 ]; then
 	  echo "file empty"
